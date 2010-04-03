e31161657f5e9ee8ed70afe33b89a645afcb4c70
Darrin Holst <darrinholst@gmail.com>
2010-04-03 00:33:29 -0500

    new icons

--- app/lib/movie-sync.js
+++ app/lib/movie-sync.js
@@ -10,7 +10,7 @@ MovieSync = {
       }
       else {
         //TODO REMOVE
-        parameters.since = "02/01/2010"
+        //parameters.since = "02/01/2010"
       }
 
       new Ajax.Request("http://reboxed.semicolonapps.com/movies", {
